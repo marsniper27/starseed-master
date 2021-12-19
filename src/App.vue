71841944bcdf7ff85af4e5ec902d5eda1db3b709
Kyle Marshall <kyle_marshall_27@hotmail.com>
2021-12-19 14:20:35 -0400

    Initializing with current codebase

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Header/>
+    
+    <router-view/>
+  </div>
+</template>
+
+
+
+
+<script>
+
+import axios from 'axios'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  },
+  data(){
+    return{
+      
+    }
+  },
+  watch: {
+   '$route' (to, from){
+    
+    }
+  },
+  methods:{
+    init(){
+    }
+  },
+  mounted(){
+  },
+  created(){
+   
+    
+  }
+}
+</script>
