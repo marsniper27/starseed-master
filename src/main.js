11021d340482662df6450caefaf058d913f4e25c
Kyle Marshall <kyle_marshall_27@hotmail.com>
2022-01-13 23:20:33 -0400

    removed store code

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@ import VueSocialSharing from 'vue-social-sharing'
 import styles from './styles.scss'
 Vue.config.productionTip = false
 Vue.use(Vuex)
-const store = new Vuex.Store({
-  state: {
-    file_url:"https://firebasestorage.googleapis.com/v0/b/howmanydaystill.appspot.com/o/"
-  }
-})
+// const store = new Vuex.Store({
+//   state: {
+//     file_url:"https://firebasestorage.googleapis.com/v0/b/howmanydaystill.appspot.com/o/"
+//   }
+// })
 
 new Vue({
   router,
-  store,
+  //store,
   render: h => h(App)
 }).$mount('#app')
 Vue.use(VueSocialSharing);
